ca627a44de0f256551ebcd5c8505063b9719ff6e
Dipesh Sinha <1606270@kiit.ac.in>
2024-02-23 02:38:05 +0530

    new

--- src/main/java/com/dipesh/instagram/config/JwtProvider.java
+++ src/main/java/com/dipesh/instagram/config/JwtProvider.java
@@ -1,8 +1,35 @@
 package com.dipesh.instagram.config;
 
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.security.core.Authentication;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
 
 public class JwtProvider {
-    private secretKey = Keys.
+    private static SecretKey key = Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
+
+    public String generateToken(Authentication auth) {
+
+        String jwt  = Jwts.builder()
+                .setIssuer("Dipesh Sinha")
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + 86400000))
+                .claim("email", auth.getName())
+                .signWith(key)
+                .compact();
+
+        return null;
+    }
+
+    public static String getEmailFromJwtToken(String jwt) {
+        //Bearer token
+        jwt = jwt.substring(7);
+
+        Claims claims = Jwts.JwtParserBuilder
+    }
 }
